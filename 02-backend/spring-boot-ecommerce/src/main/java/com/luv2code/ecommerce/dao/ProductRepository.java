14bc04364de2fa00ea76095931168bd66d2599df
nolste <nol.steiner@gmail.com>
2024-07-30 20:49:15 -0500

    initial commit

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,23 @@
+package com.luv2code.ecommerce.dao;
+
+import com.luv2code.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product,Long> {
+
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+
+    Page<Product> findByNameContaining(@Param("name") String name, Pageable pageable);
+
+    @Query("select p from Product p where p.unitsInStock = ?1 ")
+    Page<Product> findByUnitsInStock(@Param("units_in_stock") int units_in_stock, Pageable pageable);
+}

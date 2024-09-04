40212a28f343072b134bbb5b970207770b4dea58
nolste <nol.steiner@gmail.com>
2024-09-04 18:37:23 -0500

    creates cart status component

--- 03-frontend/angular-ecommerce/src/app/components/cart-status/cart-status.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/cart-status/cart-status.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../../services/cart.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cart-status',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cart-status.component.html',
   styleUrl: './cart-status.component.css'
 })
-export class CartStatusComponent {
+export class CartStatusComponent implements OnInit {
+
+  totalPrice: number = 0.00;
+  totalQuantity: number = 0;
+
+  constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.updateCartStatus();
+  }
+  updateCartStatus() {
+    //subscribe to the cart total price
+    this.cartService.totalPrice.subscribe(
+      data => this.totalPrice = data
+    )
+
+    this.cartService.totalQuantity.subscribe(
+      data => this.totalQuantity = data
+    )
+  }
+
 
 }

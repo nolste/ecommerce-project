fef220f4149aa9a470cb79aa72c97c144f3d4665
nolste <nol.steiner@gmail.com>
2024-09-09 15:58:51 -0500

    Creates cart details component

---
+++ 03-frontend/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { CartStatusComponent } from '../cart-status/cart-status.component';
+import { RouterLink } from '@angular/router';
+import { CartItem } from '../../common/cart-item';
+import { CartService } from '../../services/cart.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-cart-details',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './cart-details.component.html',
+  styleUrl: './cart-details.component.css'
+})
+export class CartDetailsComponent implements OnInit {
+
+  cartItems: CartItem[] = [];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
+  constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.listCartDetails();
+  }
+
+  listCartDetails() {
+    //get a hangle to the cart items
+    this.cartItems = this.cartService.cartItems;
+    //subscribe to the cart totalPrice, quantity and totals
+    this.cartService.totalPrice.subscribe(
+      data => this.totalPrice = data
+    );
+
+    this.cartService.totalQuantity.subscribe(
+      data => this.totalQuantity = data
+    );
+
+    this.cartService.computeCartTotals();
+
+
+  }
+
+  incrementQuantity(cartItem: CartItem) {
+    this.cartService.addToCart(cartItem);
+  }
+
+  decrementQuantity(cartItem: CartItem) {
+    this.cartService.decrementQuantity(cartItem);
+  }
+
+  remove(cartItem: CartItem) {
+    this.cartService.remove(cartItem);
+  }
+
+}

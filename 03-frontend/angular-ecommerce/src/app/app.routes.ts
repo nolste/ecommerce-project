1784d0d0a632c289f7d4d8a975e3de0b4fb7ed6c
nolste <nol.steiner@gmail.com>
2024-09-09 16:01:14 -0500

    Adds cart details component to routes

--- 03-frontend/angular-ecommerce/src/app/app.routes.ts
+++ 03-frontend/angular-ecommerce/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 
 export const routes: Routes = [
     { path: 'products/:id', component: ProductDetailsComponent },
+    { path: 'cart-details', component: CartDetailsComponent },
     { path: 'search/:keyword', component: ProductListComponent },
     { path: 'category/:id', component: ProductListComponent },
     { path: 'category', component: ProductListComponent },

9c5fe388e6a93cb23304961bba6a6b25cbe0ef3d
nolste <nol.steiner@gmail.com>
2024-11-02 17:49:42 -0500

    adds months, years, and location info for cc

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ShopFormService } from '../../services/shop-form.service';
+import { ICountry, IState } from 'countries-states-cities';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, RouterOutlet,],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css'
 })
@@ -16,7 +19,14 @@ export class CheckoutComponent implements OnInit {
   totalPrice: number = 0;
   totalQuantity: number = 0;
 
-  constructor(private formBuilder: FormBuilder) { }
+  creditCardYears: number[] = [];
+  creditCardMonths: number[] = [];
+
+  countries: ICountry[] = [];
+  states: IState[] = [];
+
+  constructor(private formBuilder: FormBuilder,
+    private shopFormSevice: ShopFormService) { }
 
   ngOnInit(): void {
     this.checkoutFormGroup = this.formBuilder.group({
@@ -48,6 +58,26 @@ export class CheckoutComponent implements OnInit {
         expirationYear: ['']
       }),
     });
+
+    //populate credit card months
+    const startMonth: number = new Date().getMonth() + 1;
+    console.log("startMonth: " + startMonth)
+    this.handleCountries();
+
+    this.shopFormSevice.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit card months: " + JSON.stringify(data));
+        this.creditCardMonths = data;
+      }
+    );
+
+    //populate credit card years
+    this.shopFormSevice.getCreditCardYears().subscribe(
+      data => {
+        console.log("Retrieved credit card year: " + JSON.stringify(data));
+        this.creditCardYears = data;
+      }
+    )
   }
 
   copyShippingAddressToBillingAddress(event: any) {
@@ -57,6 +87,62 @@ export class CheckoutComponent implements OnInit {
     else {
       this.checkoutFormGroup.controls['billingAddress'].reset();
     }
+  }
+  handleMonthsAndYears() {
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(creditCardFormGroup?.value.expirationYear);
+
+    let startMonth: number;
+
+    if (currentYear == selectedYear) {
+      startMonth = new Date().getMonth() + 1;
+    } else {
+      startMonth = 1;
+    }
+
+    this.shopFormSevice.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit card months: " + JSON.stringify(data));
+        this.creditCardMonths = data;
+      }
+    )
+  }
+
+  handleCountries() {
+    //const creditCardFormGroup = this.checkoutFormGroup.get('shippingAddress');
+    console.log("handle countries test");
+    this.shopFormSevice.getAllCountries().subscribe(
+      data => {
+        console.log("Retrieved countries: " + JSON.stringify(data));
+        this.countries = data;
+      }
+    )
+  }
+
+  handleStates(formGroupName: string) {
+    console.log("handle countries test");
+
+    const FormGroup = this.checkoutFormGroup.get(formGroupName);
+    const countryCode = FormGroup?.value.country.id;
+    console.log("country code: " + countryCode);
+
+    this.shopFormSevice.getState(countryCode).subscribe(
+      data => {
+        console.log("retrieved state: " + JSON.stringify(data));
+        this.states = data;
+      }
+    )
+
+
+
+    /*     this.shopFormSevice.getState(countryID).subscribe(
+          data => {
+            console.log("Retrieved state: " + JSON.stringify(data));
+            this.state = data;
+          }
+        ) */
   }
 
   onSubmit() {
